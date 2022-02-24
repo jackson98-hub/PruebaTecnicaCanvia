12e623b8c202ba3b998aead5693d47d519e239ca
Daniel Quezada <danielqg93@gmail.com>
2022-02-23 23:16:13 -0500

    Cambios

--- WebApi/Data/Conexion.cs
+++ WebApi/Data/Conexion.cs
@@ -7,6 +7,7 @@ namespace WebApi.Data
 {
     public class Conexion
     {
+        //COMENTARIO DE CONEXION
         public static string rutaConexion = "data source=(local)\\SQLEXPRESS; user=sa; password=123456; Initial Catalog =DBPRUEBAS; MultipleActiveResultSets=true";
     }
 }
