49eb38bb25681398155c31f79bec9320abbc2e9a
Daniel Quezada <danielqg93@gmail.com>
2022-02-23 23:34:19 -0500

    Cambios de daniel

--- WebApi/Data/Conexion.cs
+++ WebApi/Data/Conexion.cs
@@ -10,5 +10,6 @@ namespace WebApi.Data
         //COMENTARIO DE CONEXION
         public static string rutaConexion = "data source=(local)\\SQLEXPRESS; user=sa; password=123456; Initial Catalog =DBPRUEBAS; MultipleActiveResultSets=true";
         // COMENTARIO DE PAPU
+		// 01011
     }
 }
